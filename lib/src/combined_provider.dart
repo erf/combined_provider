0d5f8e69c215c3185b98429ea2cabfd3e100338a
Erlend Lind Madsen <erlendf80@gmail.com>
2021-01-11 21:41:43 +0100

    move src to lib/src and export

---
+++ lib/src/combined_provider.dart
@@ -0,0 +1,55 @@
+import 'package:riverpod/riverpod.dart';
+
+final loadedProvider =
+    Provider.family<bool, List<ProviderBase>>((ref, providerList) {
+  for (final provider in providerList) {
+    if (provider is FutureProvider) {
+      if (ref.watch(provider) is! AsyncData) {
+        return false;
+      }
+    } else if (provider is StreamProvider) {
+      if (ref.watch(provider) is! AsyncData) {
+        return false;
+      }
+    }
+  }
+  return true;
+});
+
+final combinedProvider =
+    Provider.family<AsyncValue, List<ProviderBase>>((ref, providerList) {
+  // watch for errors
+  for (final provider in providerList) {
+    final value = ref.watch(provider);
+    if (provider is FutureProvider) {
+      if (value is AsyncError) {
+        return value;
+      }
+    } else if (provider is StreamProvider) {
+      if (value is AsyncError) {
+        return value;
+      }
+    }
+  }
+
+  // else watch for loading
+  for (final provider in providerList) {
+    final value = ref.watch(provider);
+    if (provider is FutureProvider) {
+      if (value is AsyncLoading) {
+        return value;
+      }
+    } else if (provider is StreamProvider) {
+      if (value is AsyncLoading) {
+        return value;
+      }
+    }
+  }
+
+  // else collect data
+  final result = [];
+  for (final provider in providerList) {
+    result.add(ref.watch(provider));
+  }
+  return AsyncData(result);
+});
